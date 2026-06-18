f8643bf901249a232f4bd0a036ea7187720f5e96
Sahil Barve <sahilbarve1000@gmail.com>
2026-06-18 12:06:05 +0530

    Hyperparameter Trainig

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -49,14 +49,61 @@ class ModelTrainer:
                 "CatBoost Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
+            #Now we will try to apply various different parameters in order to make our model more optimal
+            params = {
+    "Decision Tree": {
+        "criterion": ["squared_error", "friedman_mse"],
+        "max_depth": [5, 10, 15, None],
+        "min_samples_split": [2, 5, 10],
+        "min_samples_leaf": [1, 2, 4]
+    },
 
+    "Random Forest": {
+        "n_estimators": [100, 200, 300],
+        "max_depth": [10, 20, None],
+        "min_samples_split": [2, 5, 10],
+        "min_samples_leaf": [1, 2, 4]
+    },
+
+    "Gradient Boosting": {
+        "n_estimators": [100, 200],
+        "learning_rate": [0.01, 0.05, 0.1],
+        "max_depth": [3, 5, 7],
+        "subsample": [0.8, 1.0]
+    },
+
+    "Linear Regression": {
+        # No important hyperparameters to tune
+    },
+
+    "XGB Regressor": {
+        "n_estimators": [100, 200],
+        "learning_rate": [0.01, 0.1],
+        "max_depth": [3, 5, 7],
+        "subsample": [0.8, 1.0],
+        "colsample_bytree": [0.8, 1.0]
+    },
+
+    "CatBoost Regressor": {
+        "iterations": [100, 200],
+        "learning_rate": [0.01, 0.1],
+        "depth": [4, 6, 8]
+    },
+
+    "AdaBoost Regressor": {
+        "n_estimators": [50, 100, 200],
+        "learning_rate": [0.01, 0.1, 1.0],
+        "loss": ["linear", "square"]
+    }
+}
             # We will write our evaluate_model function in utils file
             model_report: dict = evaluate_model(
                 X_train=X_train,
                 y_train=y_train,
                 X_test=X_test,
                 y_test=y_test,
-                models=models
+                models=models,
+                param=params
             )
 
             # To get best model score from dict
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import dill # Used in saving a pickle file
 from sklearn.metrics import r2_score
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
   try:
@@ -18,20 +19,32 @@ def save_object(file_path,obj):
   except Exception as e:
     raise CustomException(e,sys)
 
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models,param):
     try:
         report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-        for model_name, model in models.items():
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-            model.fit(X_train, y_train)
+            model.set_params(**gs.best_params_)#In Python, ** is used to unpack a dictionary into keyword arguments. all the things in the best_params are feed to model as argument parameters
+            model.fit(X_train,y_train)
+            # Train model
+            #model.fit(X_train,y_train)
 
-            y_test_pred = model.predict(X_test)
+            # Predict Training data
+            y_train_pred = model.predict(X_train)
 
-            test_model_score = r2_score(y_test, y_test_pred)
+            # Predict Testing data
+            y_test_pred =model.predict(X_test)
 
-            report[model_name] = test_model_score
+            # Get R2 scores for train and test data
+            train_model_score = r2_score(y_train,y_train_pred)
+            test_model_score = r2_score(y_test,y_test_pred)
 
+            report[list(models.keys())[i]] =  test_model_score
         return report
 
     except Exception as e:
